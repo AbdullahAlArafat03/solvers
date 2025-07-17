b3a28b6bc12b9adfb02665fb9f0d98c50815bf53
AbdullahAlArafat03 <s202156290@kfupm.edu.sa>
2025-07-17 17:19:14 +0300

    Update solver_react.py

--- solver_react.py
+++ solver_react.py
@@ -1,51 +1,65 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from geopy.distance import geodesic
 from ortools.constraint_solver import pywrapcp
 from ortools.constraint_solver import routing_enums_pb2
 
 npm run build
 
-function MyButton() {
-  return (
-    <button>I'm a button</button>
-  );
-}
 
-function AboutPage() {
-  return (
-    <>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
-    </>
-  );
-}
+solver = FastAPI()
 
-export default function MyApp() {
-  return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
-  );
-}
 
-const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-};
+def distanceMatrix (location)
 
-export default function Profile() {
-  return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }}
-      />
-    </>
-  );
-}
+
+def ortoolsSolver (distanceMatrix)
+
+
+
+
+def distance_matrix(locations):
+    size = len(locations)
+    matrix = []
+    for i in range(size):
+        row = []
+        for j in range(size):
+            dist = geodesic(locations[i], locations[j]).km
+            row.append(int(dist * 1000))  
+        matrix.append(row)
+    return matrix
+
+
+def ortools_solver(distance_matrix, num_vehicles, depot_index=0):
+    manager = pywrapcp.RoutingIndexManager(len(distance_matrix), num_vehicles, depot_index)
+    routing = pywrapcp.RoutingModel(manager)
+
+    def distance_callback(from_index, to_index):
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return distance_matrix[from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+
+    solution = routing.SolveWithParameters(search_parameters)
+    return routing, manager, solution
+
+def get_routes(routing, manager, solution, num_vehicles):
+    routes = []
+    for vehicle_id in range(num_vehicles):
+        index = routing.Start(vehicle_id)
+        route = []
+        while not routing.IsEnd(index):
+            node = manager.IndexToNode(index)
+            route.append(node)
+            index = solution.Value(routing.NextVar(index))
+        route.append(manager.IndexToNode(index))  # Add the final stop
+        routes.append(route)
+    return routes
+
+
+
